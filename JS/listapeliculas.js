e802da2a04f43e749d3a5b2b6474a689d42c728a
Yago <yaguitor@gmail.com>
2024-05-21 13:31:03 +0200

    Pantalla de película provisional

---
+++ JS/listapeliculas.js
@@ -0,0 +1,4 @@
+const peliculas = [
+    toy4["Toy Story 4", "https://youtu.be/hnl5hQaMpks?si=v5GFQVXjENmBS_x9", "../IMG/toy4.jpg", "2019", "Josh Cooley", "Woody siempre ha tenido claro cuál es su labor en el mundo y cuál es su prioridad: cuidar a su dueño, ya sea Andy o Bonnie. Sin embargo, Woody descubrirá lo grande que puede ser el mundo para un juguete cuando Forky se convierta en su nuevo compañero de habitación. Los juguetes se embarcarán en una aventura de la que no se olvidarán jamás."],
+    ejemplo["TITULO", "LINK", "PORTADA", "AÑO", "DIRECTOR", "VALORACION", "SINOPSIS"],
+];
