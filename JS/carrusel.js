60989a69b5e67fd5e9786544e0477fd20e410c07
HugoOsma_ExamenGit <perdiceman@gmail.com>
2024-05-22 16:19:33 +0200

    Me falta ordenar y el nav

---
+++ JS/carrusel.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const myslide = document.querySelectorAll('.mySlider');
+    const dot = document.querySelectorAll('.dot');
+
+    let counter = 1;
+    slidefun(counter);
+
+    let timer = setInterval(autoslide, 8000);
+
+    function autoslide() {
+        counter += 1;
+        slidefun(counter);
+    }
+
+    function plusSlides(n) {
+        counter += n;
+        slidefun(counter);
+        resetTimer();
+    }
+
+    function currentSlide(n) {
+        counter = n;
+        slidefun(counter);
+        resetTimer();
+    }
+
+    function resetTimer() {
+        clearInterval(timer);
+        timer = setInterval(autoslide, 8000);
+    }
+
+    function slidefun(n) {
+        let i;
+
+        for (i = 0; i < myslide.length; i++) {
+            myslide[i].style.display = "none";
+        }
+
+        for (i = 0; i < dot.length; i++) {
+            dot[i].classList.remove('active');
+        }
+
+        if (n > myslide.length) {
+            counter = 1;
+        }
+
+        if (n < 1) {
+            counter = myslide.length;
+        }
+
+        myslide[counter - 1].style.display = "block";
+        dot[counter - 1].classList.add('active');
+    }
+
+    // Attach event listeners
+    document.querySelector('.prev').addEventListener('click', () => plusSlides(-1));
+    document.querySelector('.next').addEventListener('click', () => plusSlides(1));
+    dot.forEach((dotElement, index) => {
+        dotElement.addEventListener('click', () => currentSlide(index + 1));
+    });
+});
