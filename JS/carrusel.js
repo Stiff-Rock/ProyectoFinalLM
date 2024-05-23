873d7e50fe03128ff4f5c515493cd8dab18e08c3
HugoOsma_ExamenGit <perdiceman@gmail.com>
2024-05-23 19:21:40 +0200

    Carrusel y nav hechos falta carruseles extra y añadir funcionalidad

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -59,3 +59,16 @@ document.addEventListener('DOMContentLoaded', () => {
         dotElement.addEventListener('click', () => currentSlide(index + 1));
     });
 });
+
+const hamburger = document.querySelector(".hamburger");
+const sectionMenu = document.querySelector(".section-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    sectionMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    sectionMenu.classList.remove("active");
+}));
